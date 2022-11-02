ed23709f2bbc7546757cf5b86e08d8a841e1e8ed
Nyuw <fabien@nyuw.dev>
2022-11-02 23:00:07 +0100

    Added help command

--- src/Controller/AppController.psm1
+++ src/Controller/AppController.psm1
@@ -20,7 +20,18 @@ Class AppController
 			# Getting command from first argument
 			Switch ($Arguments[0]) {
 				'help' {
-					
+					# Writing the list of commands
+					[Console]::Write("Usage: wsl-switch [command] [arguments]`n")
+					[Console]::Write("Global commands:")
+					[Console]::Write("   help`t`t`tShow this message")
+					[Console]::Write("   config`t`tOpen the configuration file")
+					[Console]::Write("   config-update`tUpdate the configuration file content in the application")
+					[Console]::Write("`nService switch, enable the service on the entered instance name and disable it on the other one:")
+					[Console]::Write("   apache foo`t`tActivate apache on foo")
+					[Console]::Write("   mysql bar`t`tActivate mysql on bar")
+					[Console]::Write("   php baz`t`tActivate php on baz`n")
+					[Console]::Write("The services can be written comma-separated to enable them together, for example :")
+					[Console]::Write("   apache,php foo`tActivate apache and php on foo`n")
 				}
 				'config' {
 					# Opening config.yaml with the default editor
@@ -29,9 +40,6 @@ Class AppController
 				}
 				'config-update' {
 					# TODO
-				}
-				'test' {
-					[Console]::Write($Arguments)
 				}
 				Default {
 					Throw ('Invalid argument : '+ $Arguments[0])
