9e9567e8ce757a50a9f603af5e2980a11b792f21
Nyuw <fabien@nyuw.dev>
2022-11-04 21:39:26 +0100

    Added HelpController

--- src/Controller/AppController.psm1
+++ src/Controller/AppController.psm1
@@ -1,4 +1,5 @@
 Using Module '.\ConfigController.psm1'
+Using Module '.\HelpController.psm1'
 Using Module '.\ServiceController.psm1'
 Using Module '..\Config\Builder.psm1'
 Using Module '..\Config\Loader.psm1'
@@ -36,20 +37,7 @@ Class AppController
 				}
 				'^help$' {
 					# Display the command list
-					[Console]::Write("Usage: wsl-switch [command] [optional arguments]`n")
-					[Console]::Write("Global commands:")
-					[Console]::Write("   config`t`t`tOpen the configuration file")
-					[Console]::Write("   help`t`t`t`tShow this message")
-					[Console]::Write("   instances`t`t`tShow the instance list")
-					[Console]::Write("   services`t`t`tShow the service list")
-					[Console]::Write("   version`t`t`tShow the application version")
-					[Console]::Write("`nService switch, enable the service on the entered instance name and disable it on the other one:")
-					[Console]::Write("   apache foo`t`t`tActivate apache on foo")
-					[Console]::Write("   mysql bar`t`t`tActivate mysql on bar")
-					[Console]::Write("   php baz`t`t`tActivate php on baz`n")
-					[Console]::Write("The services can be written comma-separated to enable them together, for example:")
-					[Console]::Write("   apache,php foo`t`tActivate apache and php on foo")
-					[Console]::Write("   apache,mysql,php bar`t`tActivate apache, mysql and php on bar`n")
+					[HelpController]::Show()
 				}
 				'^instances$' {
 					# Display the available instance list
@@ -58,7 +46,10 @@ Class AppController
 				'^services$' {
 					# Display the available service list
 					[Console]::Write($Config.GetServices() -Join ', ')
-
+				}
+				'^status$' {
+					# Display the status of each service
+					[ServiceController]::ShowStatus($Config)
 				}
 				'^version$' {
 					# Showing the app version
@@ -70,8 +61,8 @@ Class AppController
 					If ($Config.InstanceExists($InstanceName) -ne $True) {
 						Throw ('The instance '''+ $InstanceName +''' doesn''t exist')
 					}
-					# Calling the command
-					[ServiceController]::Call($Config, $InstanceName, $Command)
+					# Execute
+					[ServiceController]::Execute($Config, $InstanceName, $Command)
 				}
 				Default {
 					Throw (''''+ $Command +''' is not a valid command, please check wsl-switch help')
---
+++ src/Controller/HelpController.psm1
@@ -0,0 +1,23 @@
+Using Module '..\Utils\Console.psm1'
+
+Class HelpController
+{
+	[Void] Static Show()
+	{
+		[Console]::Write("Usage: wsl-switch [command] [optional arguments]`n")
+		[Console]::Write("Global commands:")
+		[Console]::Write("   config`t`t`tOpen the configuration file")
+		[Console]::Write("   help`t`t`t`tShow this message")
+		[Console]::Write("   instances`t`t`tShow the instance list")
+		[Console]::Write("   services`t`t`tShow the service list")
+		[Console]::Write("   status`t`t`tShow the status of each service")
+		[Console]::Write("   version`t`t`tShow the application version")
+		[Console]::Write("`nService switch, enable the service on the entered instance name and disable it on the other one:")
+		[Console]::Write("   apache foo`t`t`tActivate apache on foo")
+		[Console]::Write("   mysql bar`t`t`tActivate mysql on bar")
+		[Console]::Write("   php baz`t`t`tActivate php on baz`n")
+		[Console]::Write("The services can be written comma-separated to enable them together, for example:")
+		[Console]::Write("   apache,php foo`t`tActivate apache and php on foo")
+		[Console]::Write("   apache,mysql,php bar`t`tActivate apache, mysql and php on bar`n")
+	}
+}
